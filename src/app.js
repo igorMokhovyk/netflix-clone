797ddadb1e28350a878ec552d15b65f8daede94d
imonoho <imonoho@gmail.com>
2021-01-16 20:55:51 -0600

    done-faq-accordion

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {JumbotronContainer} from "./containers/jumbotron";
 import {FooterContainer} from "./containers/footer";
+import {FaqsContainer} from "./components/faq";
 
 
 export default function App() {
   return (
     <>
       <JumbotronContainer/>
+      <FaqsContainer/>
       <FooterContainer />
     </>
 
--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -1,14 +1,15 @@
 import React, {useState, useContext, createContext} from "react";
-import {Container, Frame, Title, Item, Inner, Header, Body} from './styles/accordion';
-// import PropTypes from 'prop-types';
+import {Container,  Title, Item, Inner, Header, Body} from './styles/accordion';
+import PropTypes from 'prop-types';
 
-// Accordion.propTypes = {
-//   children: PropTypes.node.isRequired,
-// };
 
 const ToggleContext = createContext()
 
 export default function Accordion({children, ...resProps}) {
+
+  Accordion.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
 
   return (
     <Container {...resProps}>
@@ -18,14 +19,21 @@ export default function Accordion({children, ...resProps}) {
 }
 
 Accordion.Title = function AccourdionTitle({children, ...restProps}) {
+
+  Accordion.Title.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+
   return <Title {...restProps}>{children}</Title>
 };
 
-Accordion.Frame = function AccourdionFrame({children, ...restProps}) {
-  return <Frame {...restProps}>{children}</Frame>
-};
 
-Accordion.Item = function AccourdionItem({children, ...restProps}) {
+
+Accordion.Item = function AccordionItem({children, ...restProps}) {
+
+  Accordion.Item.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
   const [toggleShow, setToggleShow] = useState(false);
 
   return (
@@ -37,12 +45,32 @@ Accordion.Item = function AccourdionItem({children, ...restProps}) {
 
 Accordion.Header = function AccourdionHeader({children, ...restProps}) {
 
+  Accordion.Header.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+
+
   const {toggleShow, setToggleShow} = useContext(ToggleContext)
 //better use  - setToggleShow((toggleShow) => !toggleShow) - instead - setToggleShow(!toggleShow)
-  return <Header onClick={() => setToggleShow((toggleShow) => !toggleShow)}{...restProps}>{children}</Header>
+  return (
+    <Header
+      onClick={() => setToggleShow((toggleShow) => !toggleShow)}{...restProps}>
+      {children}
+      {toggleShow ? (
+        <img src="/images/icons/close-slim.png" alt="Close" />
+        ) : (
+          <img src="/images/icons/add.png" alt="Open" />
+      )}
+    </Header>
+  );
 };
 
 Accordion.Body = function AccordionBody({children, ...restProps}) {
+
+  Accordion.Body.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+
   const {toggleShow} = useContext(ToggleContext);
 
   return toggleShow ? <Body {...restProps}>{children}</Body> : null;
